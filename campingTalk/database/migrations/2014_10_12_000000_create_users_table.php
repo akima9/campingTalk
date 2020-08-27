faf819b984dc87c5c5f5da6ca5e1f96a113939aa
akima9 <akima9a@gmail.com>
2020-08-27 20:56:49 +0900

    user DB create

--- campingTalk/database/migrations/2014_10_12_000000_create_users_table.php
+++ campingTalk/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,19 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->bigIncrements('user_cd')->comment('유저코드');
+            $table->string('user_id')->comment('아이디');
+            $table->string('user_pw')->comment('비밀번호');
+            $table->string('user_name')->comment('유저이름');
+            $table->string('user_email')->comment('이메일');
+            $table->string('user_phone')->comment('연락처');
+            $table->enum('user_level', ['client', 'admin'])->comment('회원등급');
+            $table->dateTime('create_date')->comment('가입일');
+            $table->dateTime('modify_date')->comment('수정일');
+            $table->enum('use_yn', ['Y', 'N'])->comment('사용유무');
+
+            $table->unique('user_id');
+            $table->index(['create_date','modify_date','use_yn']);
         });
     }
 
