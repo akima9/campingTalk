d67f1548af1867f20718c3301bb5b8693174c40f
akima9 <akima9a@gmail.com>
2020-09-09 21:16:58 +0900

    폰트 테스트

--- campingTalk/resources/views/layout.blade.php
+++ campingTalk/resources/views/layout.blade.php
@@ -2,29 +2,34 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <!-- bootstrap css -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+        <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+
+        <style>
+            .title{
+                font-family: 'Lobster', cursive;
+                margin-top: 20px;
+            }
+        </style>
         
         <!-- bootstrap js -->
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
-        <script src="../js/common.js"></script>
-
         <title>@yield('title','campingTalk')</title>
     </head>
     <body>
         <div class="container">
             <div class="row">
-                <!-- 로고 -->
-                <div class="col-md-4">
-                    <h1><a href="/">캠핑톡</a></h1>
+                <div class="col text-center title">
+                    <h1><a href="/">CampingTalk</a></h1>
                 </div>
-                <!-- top menu -->
                 <div class="col-md-4 ml-auto">
                     <ul class="nav justify-content-end">
                         <li class="nav-item">
@@ -39,7 +44,6 @@
                     </ul>
                 </div>
             </div>
-            <!-- navigation -->
             <nav class="navbar navbar-expand navbar-dark bg-primary">
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto">
@@ -61,7 +65,6 @@
                     </ul>
                 </div>
             </nav>
-            <!-- contents -->
             <p class="h2">@yield('subTitle')</p>
             @yield('content')
         </div>
